97cbb874b60d99c229ce4a5210a3e3aa08868069
Ben Lang <bl3rune@users.noreply.github.com>
2021-02-16 22:43:03 +0000

    Update pinger.sh

--- pinger.sh
+++ pinger.sh
@@ -8,10 +8,10 @@ fi
 
 echo "Started pinging URL: ${URL}"
 
-RESPONSE = ""
-PREVIOUS_RESPONSE = ""
+RESPONSE=""
+PREVIOUS_RESPONSE=""
 while true; do
-	PREVIOUS_RESPONSE = $RESPONSE
+	PREVIOUS_RESPONSE=$RESPONSE
 	RESPONSE=$(curl -s -k "$URL" & wait)
 	
 	if [ -z RESPONSE ] || [ RESPONSE != PREVIOUS_RESPONSE ]; then
