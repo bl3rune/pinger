dcb8eda1e5e9152c636ab90a1eb7c20888eda816
Ben Lang <bl3rune@users.noreply.github.com>
2021-02-17 15:31:57 +0000

    Add timestamps

--- pinger.sh
+++ pinger.sh
@@ -5,15 +5,16 @@ if [ -z "$URL" ]; then
 	echo '$URL missing'
 	exit 1
 fi
-
-echo "Started pinging URL: ${URL}"
+CUR_TIME=$(date "+%Y.%m.%d-%H.%M.%S")
+echo "Started pinging URL: ${URL} at ${CUR_TIME}"
 
 RESPONSE=""
 PREVIOUS_RESPONSE=""
 while true; do
 	RESPONSE=$(curl -s -S -k "$URL" & wait)
 	if [ -z "${RESPONSE}" ] || [ "${RESPONSE}" != "${PREVIOUS_RESPONSE}" ]; then
-		echo "Response changed: ${RESPONSE} from ${PREVIOUS_RESPONSE}"
+		CUR_TIME=$(date "+%Y.%m.%d-%H.%M.%S")
+		echo "Response changed: ${RESPONSE} from ${PREVIOUS_RESPONSE} at ${CUR_TIME}"
 	fi
 
 	# Sleep and loop
